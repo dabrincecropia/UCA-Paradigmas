773074e5c68959c8e6d0161ed94a0f44605ef70e
Artiogit <99515324+Artiogit@users.noreply.github.com>
2024-02-05 19:17:52 -0600

    Allan Mora Tarea

---
+++ async.js
@@ -0,0 +1,38 @@
+//Declaracion de variables para los calculos siguientes //
+
+lado = 10;
+longitud = 10;
+ancho = 15;
+base = 5;
+altura = 10;
+radio = 20;
+
+// Funciones de operacion //
+
+AreaCuadrado = lado * lado;
+PerimetroRectangulo = longitud * 2 + ancho * 2;
+AreaTriangulo = (base * altura)/2;
+LongitudCircunferencia = 2*3.14 * radio;
+AreaCirculo = 3.14 * (radio * 2);
+
+// Funciones de mostrar resultados //
+
+// Calcular el Area de un Cuadrado //
+
+console.log(AreaCuadrado);
+
+// Calcular el Perimetro de un Rectangulo //
+
+console.log(PerimetroRectangulo);
+
+// Calcular el area de un triangulo //
+
+console.log(AreaTriangulo);
+
+// Calcular la Longitud de la Circunferencia //
+
+console.log(LongitudCircunferencia);
+
+// Calcular el area del Circulo //
+
+console.log(AreaCirculo);

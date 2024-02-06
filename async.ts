e88e9accf00bb25cc33fe0532bf7fd7d2803fb26
DiegoSolano2715 <63468620+DiegoSolano2715@users.noreply.github.com>
2024-02-05 21:20:11 -0600

    DiegoSolanoTarea2

---
+++ async.ts
@@ -0,0 +1,147 @@
+/*Este es un trabajo realizado por Diego Andrés Solano Astúa
+para el profesor Dabrin Najera Barboza en la asignatura de Paradigmas y
+Pruebas de Software
+*/
+
+/* Apartado de variables a realizar para Obtener y mostrar
+el calculo de las áreas y perimetros solicitados */
+
+//Variable para el lado de un cuadrado utilizada en formula de área.
+var lado:number;
+
+/*Variable ancho y largo que se utilizaran para el perimetro de un rectangulo*/
+let ancho:number;
+var largo:number;
+
+/*Variable base y altura que se utilizaran para el perimetro de un triangulo */
+var altura:number;
+var base:number; 
+
+/*Valor del Radio para el area y longitud del circulo*/ 
+var radio:number;
+
+
+//Ahora declaramos una constante de tipo boolean para el valor PI
+const PI:number = 3.1416;
+
+
+
+/* Apartado de funciones a realizar para Obtener el calculo de las áreas y perimetros solicitados */
+//Calculo de area de un cuadrado
+/*Está función es realizada con el objetivo de obtener el cálculo del área de un cuadrado
+ya que un cuadrado tiene todos sus lados iguales para sacar su área haré un cálculo de lado * lado*/ 
+function obtenerAreaCuadrado(lado:number):number{
+    return lado * lado;
+    
+}
+  
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+//Calculo de Perimetro de un Rectangulo
+/*Está función es realizada con el objetivo de obtener el cálculo del perimetro de un rectangulo
+ para sacar su área haré un cálculo de lado + ancho + lado + ancho */ 
+function obtenerPerimetroRectangulo(largo:number,ancho:number):number{
+    
+    return largo + ancho + largo + ancho;
+    
+    }
+ 
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+
+/*Está función es realizada con el objetivo de obtener el cálculo del area de un triangulo
+ para sacar su área haré un cálculo de (base * altura)/2*/ 
+function obtenerAreaTriangulo(base:number,altura:number):number{
+   
+    return (base * altura)/2;
+    
+}
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/*Está función es realizada con el objetivo de obtener el cálculo del longitud de circunferencia
+para sacar su área haré un cálculo de 2* PI * RADIO */ 
+
+function obtenerLongitudCircunferenciaCirculo(radio:number):number{
+    return 2 * PI * radio;
+    
+    }
+
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/*Está función es realizada con el objetivo de obtener el cálculo del longitud de circunferencia
+para sacar su área haré un cálculo de PI * (RADIO*RADIO) */ 
+
+function obtenerAreaCirculo(radio:number):number{
+
+    return PI * radio * radio;
+
+}
+
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/* Apartado de funciones a realizar para Mostrar el calculo de las áreas y perimetros solicitados */
+ /* Está función es realizada con el objetivo de mostrar el resultado del área de un cuadrado, en este
+    caso tomará un valor determinado como 10 y el resultado del mismo serán 100*/
+    function mostrarResultadoCuadrado():void{
+        console.log('Mostrar Area Cuadrado: ' + obtenerAreaCuadrado(10));
+        
+    }
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+/* Está función es realizada con el objetivo de mostrar el resultado del perimetro de rectangulo, en este
+    caso tomará un valor determinado como largo 4 y ancho 5 y el resultado del mismo serán 18*/
+    function mostrarResultadoRectangulo():void{
+        console.log('Mostrar Perimetro Rectangulo: ' + obtenerPerimetroRectangulo(4,5));
+        
+    }
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/* Está función es realizada con el objetivo de mostrar el resultado del área de un triangulo, en este
+    caso tomará un valor determinado como 4 base y 5 altura y el resultado del mismo serán 10*/
+    function mostrarResultadoTriangulo():void{
+        console.log('Mostrar Area Triangulo: ' + obtenerAreaTriangulo(4,5));
+        
+    }
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+ /* Está función es realizada con el objetivo de mostrar el resultado del la longitud de la
+ circunferencia de un circulo, en estecaso tomará un valor determinado como 31.416 
+*/
+    function mostrarResultadosLongitudCircunferencia():void{
+        console.log('Mostrar Resultado Longitud Circulo: ' + obtenerLongitudCircunferenciaCirculo(5));
+        
+    }
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+/* Está función es realizada con el objetivo de mostrar el resultado del area de un circulo
+, en estecaso tomará un valor determinado como 78.54
+*/
+ function mostrarResultadosAreaCircunferencia():void{
+        console.log('Mostrar Resultado Area Circulo: ' + obtenerAreaCirculo(5));
+        
+    }
+/*-----------------------------------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------------------*/
+
+//LLamado de cada función de visualización
+mostrarResultadoCuadrado();
+mostrarResultadoRectangulo();
+mostrarResultadoTriangulo();
+mostrarResultadosLongitudCircunferencia();
+mostrarResultadosAreaCircunferencia();

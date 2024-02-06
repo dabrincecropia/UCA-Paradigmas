37472ea091ecd64dfe44e64f4d4248e556b5ca02
Artiogit <99515324+Artiogit@users.noreply.github.com>
2024-02-06 17:22:20 -0600

    Update async.js

--- async.js
+++ async.js
@@ -1,4 +1,4 @@
-//Declaracion de variables para los calculos siguientes //
+///Declaracion de variables para los calculos siguientes //
 
 lado = 10;
 longitud = 10;
@@ -6,33 +6,49 @@ ancho = 15;
 base = 5;
 altura = 10;
 radio = 20;
+pi = 3.14
 
 // Funciones de operacion //
 
-AreaCuadrado = lado * lado;
-PerimetroRectangulo = longitud * 2 + ancho * 2;
-AreaTriangulo = (base * altura)/2;
-LongitudCircunferencia = 2*3.14 * radio;
-AreaCirculo = 3.14 * (radio * 2);
+function AreaCuadrado(lado) {
+    return lado * lado;
+}
+
+function PerimetroRectangulo (longitud,ancho){
+    return longitud *2 +ancho *2;
+}
+
+function AreaTriangulo (base,altura) {
+    return (base*altura)/2;
+}
+
+function LongitudCircunferencia (pi,radio) {
+    return (2*pi) * radio;
+}
+
+
+function AreaCirculo (pi,radio) {
+    return pi*(radio*2);
+}
 
 // Funciones de mostrar resultados //
 
 // Calcular el Area de un Cuadrado //
 
-console.log(AreaCuadrado);
+console.log(AreaCuadrado(lado));
 
 // Calcular el Perimetro de un Rectangulo //
 
-console.log(PerimetroRectangulo);
+console.log(PerimetroRectangulo(longitud,ancho));
 
 // Calcular el area de un triangulo //
 
-console.log(AreaTriangulo);
+console.log(AreaTriangulo(base,altura));
 
 // Calcular la Longitud de la Circunferencia //
 
-console.log(LongitudCircunferencia);
+console.log(LongitudCircunferencia(pi,radio));
 
 // Calcular el area del Circulo //
 
-console.log(AreaCirculo);
+console.log(AreaCirculo(pi,radio));
